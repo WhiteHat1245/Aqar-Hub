8efbd85df351e9ab81c427123448e060e13bbe47
WhiteHat1245 <zaferali90@gmail.com>
2026-07-13 20:21:06 +0200

    fix: correct hosted API subdomain in Constants.kt

--- android/app/src/main/java/com/example/aqarhub/utils/Constants.kt
+++ android/app/src/main/java/com/example/aqarhub/utils/Constants.kt
@@ -1,8 +1,9 @@
 package com.example.aqarhub.utils
 
 object Constants {
-    const val BASE_URL = "https://aqar-hub-api.onrender.com/"
+    const val BASE_URL = "https://aqar-hub.onrender.com/"
 }
+
 
 
 
